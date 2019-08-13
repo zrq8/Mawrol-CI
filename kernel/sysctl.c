26afc193cc21fc60afb1943f9a06439a1ceed76d
Danny Lin <danny@kdrag0n.dev>
2019-08-13 06:11:14 +0000

    Revert "sysctl: Nuke sched_boost feature"
    This reverts commit 040dca0e4375253f88120966434d56ce708980c9.
    
    Revert this in favor of no-oping the sched_boost node instead to prevent
    perfd from complaining in logcat.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -375,7 +375,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &neg_three,
 		.extra2		= &three,
 	},
-#if 0
 	{
 		.procname	= "sched_walt_rotate_big_tasks",
 		.data		= &sysctl_sched_walt_rotate_big_tasks,
@@ -385,7 +384,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
-#endif
 	{
 		.procname	= "sched_min_task_util_for_boost",
 		.data		= &sysctl_sched_min_task_util_for_boost,
